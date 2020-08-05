64ec65cc771768fa12ea5c184a12fa7ea84980c6
Matthew Gleich <matthewgleich@gmail.com>
2020-08-05 13:31:02 -0400

    ✨ Init script creates go mod
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/Matt-Gleich/PROJECT_NAME
-
-go 1.14
--- init.py
+++ init.py
@@ -23,34 +23,38 @@ def ls_files(dir):
             else:
                 files.append(abspath)
         except FileNotFoundError as err:
-            print('invalid directory\n', 'Error: ', err)
+            print("invalid directory\n", "Error: ", err)
     return files
 
 
-project_name = input('What is the name of this project?\n').lower()
-project_description = input(
-    '\nWhat is the description for this project?\n').lower()
+project_name = input("What is the name of this project?\n").lower()
+project_description = input("\nWhat is the description for this project?\n").lower()
 
 cleaned_files = []
-for filename in ls_files('.'):
+for filename in ls_files("."):
     ignore = False
-    if not (filename.startswith(
-            './.git') or '__pycache__' in filename or '.pytest_cache' in filename or filename == './init.py'):
+    if not (
+        filename.startswith("./.git")
+        or "__pycache__" in filename
+        or ".pytest_cache" in filename
+        or filename == "./init.py"
+    ):
         cleaned_files.append(filename)
 
 for filename in cleaned_files:
-    with open(filename, 'r') as file:
+    with open(filename, "r") as file:
         cleaned_lines = []
         lines = file.readlines()
         for line in lines:
             cleaned_lines.append(
-                line.replace(
-                    'PROJECT_NAME',
-                    project_name).replace(
-                    'PROJECT_DESCRIPTION',
-                    project_description)
+                line.replace("PROJECT_NAME", project_name).replace(
+                    "PROJECT_DESCRIPTION", project_description
+                )
             )
-    with open(filename, 'w') as file:
-        file.write(''.join(cleaned_lines))
+    with open(filename, "w") as file:
+        file.write("".join(cleaned_lines))
 
-print('Everything filled in!\nPlease now delete this file!')
+os.system(f"go mod init github.com/Matt-Gleich/{project_name}")
+
+print("Everything filled in!\nPlease now delete this file!")
+
